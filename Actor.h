275109f94db85a57e3d4468e2f065841790f7dfb
Skelkingr <aymeric.goupil@gmail.com>
2022-10-06 00:04:35 +0200

    Update Actor and Component base classes.

--- Actor.h
+++ Actor.h
@@ -1,8 +1,34 @@
 #pragma once
 
+#include <vector>
+
+#include "Component.h"
+#include "Game.h"
+
 class Actor
 {
 public:
-	virtual void Update(float deltaTime);
-	virtual void Draw();
+	enum State
+	{
+		EActive,
+		EPaused,
+		EDead
+	};
+public:
+	Actor(Game* game);
+	virtual ~Actor();
+
+	void Update(float deltaTime);
+	void UpdateComponents(float deltaTime);
+	virtual void UpdateActor(float deltaTime);
+
+	void AddComponent(Component* component);
+	void RemoveComponent(Component* component);
+private:
+	State mState;
+	Vector2 mPosition;
+	float mScale;
+	float mRotation;
+	std::vector<Component*> mComponents;
+	Game* mGame;
 };
--- Component.h
+++ Component.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include "Actor.h"
+
 class Component
 {
+public:
+	Component(Actor* owner, int updateOrder = 100);
+	virtual ~Component();
 
+	virtual void Update(float deltaTime);
+	int GetUpdateOrder() const { return mUpdateOrder; }
+protected:
+	Actor* mOwner;
+	int mUpdateOrder;
 };
