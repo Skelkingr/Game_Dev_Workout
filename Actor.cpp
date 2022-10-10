b5a04b84456e0adb07722cdad3fbbf3d51d6cfdd
Skelkingr <aymeric.goupil@gmail.com>
2022-10-10 16:28:27 +0200

    Update Actor.cpp with new method defs.

--- Actor.cpp
+++ Actor.cpp
@@ -22,3 +22,47 @@ Actor::~Actor()
 		delete mComponents.back();
 	}
 }
+
+void Actor::Update(float deltaTime)
+{
+	if (mState == EActive)
+	{
+		UpdateComponents(deltaTime);
+		UpdateActor(deltaTime);
+	}
+}
+
+void Actor::UpdateComponents(float deltaTime)
+{
+	for (auto comp : mComponents)
+	{
+		comp->Update(deltaTime);
+	}
+}
+
+void Actor::AddComponent(Component* component)
+{
+	int myOrder = component->GetUpdateOrder();
+
+	auto iter = mComponents.begin();
+	for (;
+		iter != mComponents.end();
+		++iter)
+	{
+		if (myOrder < (*iter)->GetUpdateOrder())
+		{
+			break;
+		}
+	}
+
+	mComponents.insert(iter, component);
+}
+
+void Actor::RemoveComponent(Component* component)
+{
+	auto iter = std::find(mComponents.begin(), mComponents.end(), component);
+	if (iter != mComponents.end())
+	{
+		mComponents.erase(iter);
+	}
+}
